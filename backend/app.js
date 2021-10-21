0b6f2d52c99dc11d70d858fe20195421979cc934
Rémi <cabreraremi@gmail.com>
2021-10-21 14:36:27 +0200

    END

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
+const path = require('path')
 
 const app = express();
 
@@ -22,6 +24,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/stuff', stuffRoutes)
+app.use("/images", express.static(path.join(__dirname, 'images')));
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
 
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
 
-router.get('/', stuffCtrl.getAllStuff);
+router.get('/', auth, stuffCtrl.getAllStuff);
 
 
 module.exports = router;
